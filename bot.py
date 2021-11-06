0be3e0416b9ba6d04baeb1844e6913beaddca214
op1414tw <tyt066666@gmail.com>
2021-11-06 18:25:29 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+from discord import channel
+from discord.ext import commands
+from discord.flags import Intents
+
+Intents=discord.Intents.default()
+Intents.members=True
+
+bot = commands.Bot(command_prefix='!',Intents=Intents)
+
+@bot.event
+async def on_ready():
+   print(">> bot is online <<")
+
+@bot.event
+async def on_member_join(member):
+   channel=bot.get_channel(870156844541296644)
+   await channel.send(f'{member}join!')
+
+@bot.event
+async def on_member_remove(member):
+   channel=bot.get_channel(870156844541296644)
+   await channel.send(f'{member}leave!')
+
+bot.run('OTA2MzcxMTQ5MzI2MzQwMDk2.YYXp6Q.zNzdyDXZcfC1B_mIy97ropKIdho')
